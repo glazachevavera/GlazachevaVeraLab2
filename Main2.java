1a11d4077b3cb30192cefa4b1129e04a2c05ce6e
GlazachevaVera <glazacheva.vera22@yandex.ru>
2025-11-16 13:30:48 +0300

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -5,27 +5,33 @@ public class Main {
     public static PrintStream out = System.out;
     public static void main(String[] args) {
         int n = in.nextInt();
-        int [] a = new int[n];
-        for (int i = 0; i < n; i++)
-            a[i] = in.nextInt();
-        int l = 0;
-        int k = 1;
-        while (k < n && l == 0) {
-            int min = a[0];
-            int max = a[0];
-            for (int i = 1; i <= k; i++) {
-                if (a[i] > max) {
-                    max = a[i];
-                }
-                if (a[i] < min) {
-                    min = a[i];
-                }
-            }
-            if (Math.abs(min%2) == Math.abs(max%2)) {
-                l = k+1;
-            }
-            k++;
+        int min = in.nextInt();
+        int max = in.nextInt();
+        if (max < min) {
+            int a = max;
+            max = min;
+            min = a;
         }
-        out.print(l);
+        if ((max+min)%2 == 0) {
+            out.print(2);
+        }
+        else {
+            int l = 0;
+            int k = 3;
+            while (k <= n && l == 0) {
+                int x = in.nextInt();
+                if (x < min) {
+                    min = x;
+                }
+                if (x > max) {
+                    max = x;
+                }
+                if ((min+max)%2 == 0) {
+                    l = k;
+                }
+                k++;
+            }
+            out.print(l);
+        }
     }
 }
